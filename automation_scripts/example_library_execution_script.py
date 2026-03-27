564f9e0b4ef94c4ca6c97dd0de7a6077c041402a
Camden Blake <wvcamdenblake@gmail.com>
2026-03-27 15:34:44 +0000

    Update example scripts

--- automation_scripts/example_library_execution_script.py
+++ automation_scripts/example_library_execution_script.py
@@ -1,13 +1,8 @@
 from pathlib import Path
 import os
 
-# mavor_exe = Path.home() / "School/My_Programs/Mavor_Support/external/Mavor/build/mavor"
-# endf_loc = Path.home() / "School/My_Programs/Mavor_Support/Data/Libraries/ENDF_8_0"
-# working_dir = Path.home() / "School/My_Programs/Mavor_Support/Data/Test_ENDF8_0"
-# output_loc = Path.home() / "School/My_Programs/Mavor_Support/Data/Test_ENDF8_0_OTF_Files"
-
 mavor_exe = Path("..","build","mavor")
-endf_loc = Path("..","build","test_files","simple_endf_library")
+endf_loc = Path("..","build","test_files","ENDF8.0")
 working_dir = Path("working_dir")
 output_loc = Path("otf_files")
 
--- automation_scripts/example_material_execution_script.py
+++ automation_scripts/example_material_execution_script.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import os
 
 mavor_exe = Path("..","build","mavor")
-leapr_file = Path("..","build","test_files","simple_endf_library","tsl-HinH2O.leapr")
-endf_file = Path("..","build","test_files","simple_endf_library","tsl-HinH2O.endf")
+leapr_file = Path("..","build","test_files","ENDF8.0","tsl-HinH2O.leapr")
+endf_file = Path("..","build","test_files","ENDF8.0","tsl-HinH2O.endf")
 
 cmd = f"python process_single_material.py {str(mavor_exe)} {str(leapr_file)} {str(endf_file)}"
 
